ae2a53347d683a77f6cb0fb21a16c7ed98c75d71
Mikko Kokkonen <mikko@mikian.com>
2018-05-12 21:00:46 +0100

    Support calling API directly

--- lib/blubber/cli.rb
+++ lib/blubber/cli.rb
@@ -6,23 +6,6 @@ module Blubber
   class Cli < Thor
     desc 'build', 'Builds all found Docker images'
     def build
-      # Build images
-      images = layers.map { |layer| Flow.new(layer: layer).run }
-
-      table = [set_color('Layer', :bold), set_color('Tag', :bold)]
-      table += images.map do |(project, tags)|
-        tags.map do |tag|
-          [project, tag ]
-        end
-      end
-
-      puts HighLine.new.list(table.flatten, :columns_across, 2)
-    end
-
-    no_tasks do
-      def layers
-        @layers ||= Dir['**/*/Dockerfile'].map { |d| File.dirname(d) }
-      end
-    end
+      Flow.build
   end
 end
